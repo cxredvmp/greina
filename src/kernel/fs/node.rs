5d972b6d0b055eb7da454a21fea4b0e6e8c7e626
Vitalii Chyhryn <chyhryn.vitalii+edu@gmail.com>
2025-12-11 06:48:42 +0200

    fs/node: implement translation from logical to physical block index

--- src/kernel/fs/node.rs
+++ src/kernel/fs/node.rs
@@ -9,7 +9,7 @@ pub const NODE_SIZE: usize = size_of::<Node>();
 pub const NODES_PER_BLOCK: usize = BLOCK_SIZE / NODE_SIZE;
 
 /// How many extents a [Node] can have.
-const MAX_EXTENTS: usize = 15;
+const EXTENTS_PER_NODE: usize = 15;
 
 /// Represents a file system object.
 #[repr(C)]
@@ -18,14 +18,56 @@ const MAX_EXTENTS: usize = 15;
 pub struct Node {
     pub size: u64,
     pub link_count: u64,
-    pub extents: [Extent; MAX_EXTENTS],
+    pub extents: [Extent; EXTENTS_PER_NODE],
 }
 
-/// Represents a contiguous span of blocks.
+impl Node {
+    /// Resolves the logical block index into a physical block index.
+    pub fn get_physical_block(&self, logical_index: usize) -> Option<usize> {
+        let mut offset = logical_index;
+        for extent in self.extents.iter().take_while(|e| !e.is_null()) {
+            let blocks_in_extent = extent.block_count();
+            if blocks_in_extent > offset {
+                return Some(extent.start + offset);
+            }
+            offset -= blocks_in_extent;
+        }
+        None
+    }
+
+    /// Resolves the byte offset into a physical block index.
+    pub fn get_physical_block_from_offset(&self, byte_offset: usize) -> Option<usize> {
+        let logical_index = byte_offset / BLOCK_SIZE;
+        self.get_physical_block(logical_index)
+    }
+
+    /// Returns the number of logical blocks that belong to the node.
+    pub fn block_count(&self) -> usize {
+        self.extents
+            .iter()
+            .filter(|e| !e.is_null())
+            .map(|e| e.end - e.start)
+            .sum()
+    }
+}
+
+/// Represents a contiguous span of physical blocks.
 #[repr(C)]
 #[derive(Default, Clone, Copy)]
 #[derive(FromBytes, IntoBytes, Immutable)]
 pub struct Extent {
-    pub start_block: usize,
-    pub length: usize,
+    start: usize,
+    end: usize,
+}
+
+impl Extent {
+    /// Checks whether the extent does not point to any physical blocks.
+    pub fn is_null(&self) -> bool {
+        self.start == 0
+    }
+
+    /// Returns the number of blocks in this extent.
+    pub fn block_count(&self) -> usize {
+        self.end - self.start
+    }
 }
